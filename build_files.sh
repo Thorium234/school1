c24a88cf7fe19919867f93e325db9436bc75deab
Thorium234 <inyangalapaul5@gmail.com>
2025-06-03 14:12:57 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
-#python3.9 manage.py collectstatic --noinput
-# Remove the .ipynb_checkpoints directory if it exists
+
+# Run migrations (if needed)
+python manage.py migrate
+
+# Remove the .ipynb_checkpoints directory if it exists (keep this if you need it)
+find . -name ".ipynb_checkpoints" -exec rm -rf {} + || true
